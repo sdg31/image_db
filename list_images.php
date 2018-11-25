a154568341c031abeed30f2cb75b60e51c2d5b6c
samgoodrick <samgoodrick@lock3software.com>
2018-11-25 18:40:03 -0500

    Clean up list loop and thumbnail images.

--- list_images.php
+++ list_images.php
@@ -9,8 +9,8 @@
           display:inline-block;
       }
 
-      img {
-          width: 150px;
+      table, td, tr {
+          padding: 5px;          
       }
     </style>
   </head>
@@ -33,44 +33,24 @@
      $result = mysqli_query($db,$query);
 
      if (!$result) {
-       print "Error - the query could not be executed";
+       print "Error - tde query could not be executed";
        exit;
      }
 
      $num_fields = mysqli_num_fields($result);
 
-     print "<table><caption> <h2> Images ($num_fields) </h2> </caption>";
-     print "<tr align = 'center'>";
+     print "<table><tr>";
 
-     //$row = mysqli_fetch_array($result);
-     //$num_fields = mysqli_num_fields($result);
-
-     // Produce the column labels
-
-     // Output the values of the fields in the rows
-     for ($row_num = 0; $row_num < $num_rows; $row_num++) {
-       print "<tr align = 'center'>";
-       $values = array_values($row);
-       for ($index = 0; $index < $num_fields; $index++){
-         $value = htmlspecialchars($values[2 * $index + 1]);
-         $id = htmlspecialchars($values[1]);
-
-         if($index == 1) {
-                 //print "<th><form action=\"display.php\" method=\"get\">
-                 //<input type=\"text\" style=\"visibility:hidden\" name=\"id\" 
-                 //value=" . $value . "> <input type=\"image\" name=\"image\"
-                 //src=\"images/" . $value . "\"></input></form></th>";
-                 print "<th><img src=\"images/" . $value . "\"></th>";
-         }
-         else
-           print "<th>" . $value . "</th> ";
-       }
-
-     print "</tr>";
-     $row = mysqli_fetch_array($result);
+     // Display each image as a link to tde display page.
+     foreach ($result as $row) {
+       $filename = $row["Filename"];
+       print "<td><form action=\"display.php\" metdod=\"get\">
+       <input type=\"hidden\" name=\"id\"
+       value=" . $filename . "> <input type=\"image\" name=\"image\"
+       src=\"images/" . $filename . "\"></input></form></td>";
      }
 
-     print "</table>";
+     print "</tr></table>";
      ?>
   </body>
 </html>
